684f44d6e70942b274a08aa4dc362e23b2d49072
Abhisek Nanda <42082772+sentry123@users.noreply.github.com>
2023-11-29 10:16:38 +0530

    Update KafkaConsumerNormalizer.py

--- src/KafkaConsumerNormalizer.py
+++ src/KafkaConsumerNormalizer.py
@@ -4,12 +4,14 @@ from pyspark.sql.functions import explode, split
 
 spark = SparkSession.builder \
     .appName("KafkaSparkStreaming")     \
-    .config("spark.jars", "data/spark-sql-kafka-0-10_2.12-3.4.1.jar") \
+    .config("spark.jars", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.4.1") \
     .getOrCreate()
+
 
 # Define the Kafka parameters
 kafka_bootstrap_servers = "localhost:9092"
 kafka_topic = "test"
+
 
 # Define the input DataFrame representing the stream
 df = spark.readStream \
@@ -18,16 +20,20 @@ df = spark.readStream \
     .option("subscribe", kafka_topic) \
     .load()
 
+
 # Convert the binary value from Kafka into a string
 value_df = df.selectExpr("CAST(value AS STRING)")
+
 
 # Split the string into words
 words = value_df.select(
     explode(split(value_df.value, " ")).alias("word")
 )
 
+
 # Count the occurrences of each word
 word_counts = words.groupBy("word").count()
+
 
 # Display the word counts to the console
 query = word_counts \
@@ -35,6 +41,7 @@ query = word_counts \
     .outputMode("complete") \
     .format("console") \
     .start()
+
 
 # Await termination of the query
 query.awaitTermination()
